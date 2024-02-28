937ca6ba6bce6f38c4ca3dec60fe4aa336f77573
mengxiaoxuan <mengxiaoxuan@baidu.com>
2024-02-28 11:29:43 +0800

    fix: ssrInline redundance style

--- src/hooks/useStyleRegister.tsx
+++ src/hooks/useStyleRegister.tsx
@@ -493,19 +493,20 @@ export default function useStyleRegister(
 
   return (node: React.ReactElement) => {
     let styleNode: React.ReactElement;
+    const [times] = cache.get(['style', ...fullPath]) || [0];
 
-    if (!ssrInline || isMergedClientSide || !defaultCache) {
+    if (!ssrInline || isMergedClientSide || !defaultCache || (ssrInline && !isMergedClientSide && !defaultCache && times > 1)) {
       styleNode = <Empty />;
     } else {
       styleNode = (
-        <style
-          {...{
-            [ATTR_TOKEN]: cachedTokenKey,
-            [ATTR_MARK]: cachedStyleId,
-          }}
-          dangerouslySetInnerHTML={{ __html: cachedStyleStr }}
-        />
-      );
+          <style
+            {...{
+              [ATTR_TOKEN]: cachedTokenKey,
+              [ATTR_MARK]: cachedStyleId,
+            }}
+            dangerouslySetInnerHTML={{ __html: cachedStyleStr }}
+          />
+        );
     }
 
     return (
